7e9562ccd5c2622ccbe7de11b722131d734252b1
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-30 04:15:16 -0300

    def __str__ to return proper name values in the admin panel

--- meu_projeto/meuapp/models/plantao.py
+++ meu_projeto/meuapp/models/plantao.py
@@ -5,6 +5,5 @@ class Plantao(models.Model):
     nome = models.CharField(max_length=20, default="Normal")
     horario = models.CharField(max_length=10, null=True)
 
-
-def __str__(self):
-    return self.nome
+    def __str__(self):
+        return self.nome
--- meu_projeto/meuapp/models/telefone.py
+++ meu_projeto/meuapp/models/telefone.py
@@ -5,3 +5,6 @@ class Telefone(models.Model):
     ddd = models.IntegerField(default=0)
     numero = models.BigIntegerField(default=0)
     temWhatsapp = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.ddd} {self.numero}"
